e0facd02b7d78167829cc595f626d5f2989d41a5
amwanjesa <a.mwanjesa@gmail.com>
2017-05-19 10:06:58 +0200

    data mining & processing done

--- clean_wiki_data.py
+++ clean_wiki_data.py
@@ -10,11 +10,14 @@ def count(input_file, output_file):
 	id_count = 0
 	with open(input_file, 'r', encoding='utf-8') as i, open(output_file, 'w', encoding='utf-8') as o:
 		for line in tqdm(i):
-			new_line = '\t'.join(line.split())
-			o.write(new_line + ' \n')
-	# with open(output_file, 'w', encoding='utf-8') as o:
-	# 	for c in counter.most_common():
-	# 		o.write(str(c[1]) + " " + c[0] + "\n")
+			try:
+				new_line = line.replace('    GENERAL    ', '\tGENERAL\t')
+				assert line.index('    GENERAL    ')
+			except Exception as e:
+				new_line = line.replace('    PROG    ', '\tPROG\t')
+				assert line.index('    PROG    ')
+			# import pdb; pdb.set_trace()
+			o.write(new_line)
 
 
 if __name__ == "__main__":
